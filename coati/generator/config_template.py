c96434719fbc5eb01033a2054f012ca67fe702f6
Roberto Antelo <roberto_antelo@hotmail.com>
2017-04-19 19:10:58 -0500

    Implement config.py generator

---
+++ coati/generator/config_template.py
@@ -0,0 +1,3 @@
+from builder import *
+
+slides = "_-{}-_"
--- coati/generator/generator.py
+++ coati/generator/generator.py
@@ -2,7 +2,8 @@ import win32com.client as w32
 import os
 
 path = 'builders/'
-src = 'generator/slide_template.py'
+template_path = 'generator/slide_template.py'
+config_template_path = 'generator/config_template.py'
 
 def _get_slides_shapes(ppt_path):
     pptApp = w32.Dispatch('PowerPoint.Application')
@@ -44,12 +45,21 @@ def insert_code(complete_text, text_to_insert, text_to_replace):
     ans = complete_text.replace(text_to_replace, text_to_insert)
     return ans
 
-for i, slide in enumerate(_get_slides_shapes):
-    filename = path + 'slide' + str(i+1) + '.py';
+def generate(ppt_path):
     spaces = " " * 12
-    generate_path(path)
-    cp(src, filename, lambda source: insert_code(
-        source,
-        str(slide).replace(", ",",\n" + spaces),
-        '"_-{}-_"'))
+    slide_tuples = '['
+    config_filename = path + 'config.py'
+    for i, slide in enumerate(_get_slides_shapes(ppt_path)):
+        slide_name = 'slide' + str(i+1)
+        slide_tuples += ('\n' + spaces if i != 0 else '') + '(' + str(i) + ' ,' + slide_name + '.build()),'
+        filename = path + slide_name + '.py';
+        generate_path(path)
+        cp(template_path, filename, lambda source: insert_code(
+            source,
+            str(slide).replace(", ",",\n" + spaces),
+            '"_-{}-_"'))
 
+    cp(config_template_path, config_filename, lambda source: insert_code(
+        source,
+        (slide_tuples[:-1] + ']'),
+        '"_-{}-_"'))
